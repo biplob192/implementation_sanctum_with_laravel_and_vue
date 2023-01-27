6451fe7daadfae50b1895ac7aa94eb7f1d192e5a
biplob192 <biplob.net2@gmail.com>
2023-01-27 16:46:43 +0600

    FrontEnd: Delete all extra files and starting with Vuex, that will be update day-by-day.

---
+++ frontend/src/components/TestCom.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    msg: String,
+    likes: Number
+  },
+  
+  data() {
+    return{
+    data: this.$store.state.data,
+  }
+  }
+ }
+</script>
+
+<template>
+  <div>
+    <h3>You’ve successfully created a project with <span style="color: orangered;">{{ msg }}</span> </h3>
+    <b>{{ data }}</b> from HelloWorld components.
+  </div>
+</template>
+
+<style scoped></style>
