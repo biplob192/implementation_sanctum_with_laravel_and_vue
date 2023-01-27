20d0b6dd0dd92bbb20de8f66f84d49bf9c7c93e0
biplob192 <biplob.net2@gmail.com>
2023-01-27 18:07:16 +0600

    FrontEnd: Added multiple data in the state

--- frontend/src/components/TestCom.vue
+++ frontend/src/components/TestCom.vue
@@ -8,6 +8,8 @@ export default {
   data() {
     return{
     data: this.$store.state.data,
+    user: this.$store.state.user,
+    books: this.$store.state.books,
   }
   }
  }
@@ -15,8 +17,19 @@ export default {
 
 <template>
   <div>
-    <h3>You’ve successfully created a project with <span style="color: orangered;">{{ msg }}</span> </h3>
-    <b>{{ data }}</b> from HelloWorld components.
+    <h3>You've successfully created a project with <span style="color: orangered;">{{ msg }}</span> </h3>
+    <b>{{ data }}</b> from Test components.
+
+    <p>User Info:</p>
+    {{ user.name }} <br>
+    Mobile: {{ user.mobile }}<br>
+    Address: {{ user.address }}<br>
+
+    <p>Book Info:</p>
+    <li v-for="book in books">
+  {{ book.name }}, [Author: {{ book.author }}]
+</li>
+
   </div>
 </template>
 
